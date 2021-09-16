aed33867e800bdb4a332ab9a3fb421bad8f81f76
John Edward <jnarvaez@ikatechsolutions.com>
2021-09-15 19:44:02 -0500

    Initial Commit

---
+++ Shared/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  Shared
+//
+//  Created by John Edward Narvaez Londoño on 15/09/21.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
